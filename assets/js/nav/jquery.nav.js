7d1dc641ab4a169ca7d3e832896f779daaab4e1e
jaycb3 <jaycb3rd@gmail.com>
2017-09-12 16:40:40 -0500

    more

--- assets/js/nav/jquery.nav.js
+++ assets/js/nav/jquery.nav.js
@@ -144,7 +144,7 @@
 			var $link = $(e.currentTarget);
 			var $parent = $link.parent();
 			var newLoc = '#' + self.getHash($link);
-			if ($link == 'a.header-fix') {
+			if ($link === 'a.header-fix') {
 				console.log("link ->" + $link);
 				
 			} else {
